e37f6d9b92494f11d54f0a7eb246828a4bb6b311
matteomakovec <matteo.makovec@gmail.com>
2024-02-25 19:27:24 +0100

    Selection Sort

---
+++ Sorting/SelectionSort.py
@@ -0,0 +1,20 @@
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+my_list = [64, 34, 25, 12, 22, 11, 90]
+print("Array: ", my_list)
+selection_sort(my_list)
+print("Sorted array:", my_list)
+
+"""
+Time complexity: O(N^2)
+Space complexity: O(1)
+"""
